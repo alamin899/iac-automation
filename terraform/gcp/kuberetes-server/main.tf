cbc448fe6f5c1507b87871d826a286c7a0f4beab
alamin899 <alaminhossen899@gmail.com>
2025-10-13 09:21:27 +0600

    update

--- terraform/gcp/kuberetes-server/main.tf
+++ terraform/gcp/kuberetes-server/main.tf
@@ -1,16 +1,66 @@
+#########################################
+# Google Cloud Provider Configuration
+#########################################
+
 provider "google" {
   credentials = file("/Users/test/Downloads/terraform-sa.json")
   project     = "kubernetes-practice-474317"
   region      = "asia-southeast1"
 }
 
-# Use the default VPC network (Google Cloud automatically creates this network)
+#########################################
+# Use Default VPC Network
+#########################################
+
 data "google_compute_network" "default" {
   name = "default"
 }
 
-# Create Server 1
-resource "google_compute_instance" "server_1" {
+#########################################
+# Firewall Rule: Allow All Traffic
+#########################################
+
+resource "google_compute_firewall" "allow_all_ingress" {
+  name    = "allow-all-ingress"
+  network = data.google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-all"]
+}
+
+resource "google_compute_firewall" "allow_all_egress" {
+  name    = "allow-all-egress"
+  network = data.google_compute_network.default.name
+
+  allow {
+    protocol = "all"
+  }
+
+  direction          = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+  target_tags        = ["allow-all"]
+}
+
+#########################################
+# VM: Kubernetes Master Node
+#########################################
+
+resource "google_compute_instance" "kubernetes_master" {
   name         = "kubernetes-master-node-1"
   machine_type = "e2-small"
   zone         = "asia-southeast1-a"
@@ -23,76 +73,24 @@ resource "google_compute_instance" "server_1" {
 
   network_interface {
     network = data.google_compute_network.default.self_link
-    
-    access_config {}
+    access_config {} # enables public IP
   }
 
-  tags = ["http-server", "https-server"]
+  tags = ["http-server", "https-server", "allow-all"]
 
   metadata = {
-    ssh-keys = file("/Users/test/.ssh/id_ed25519.pub")
+    ssh-keys = "alamin:${file("/Users/test/.ssh/id_ed25519.pub")}"
   }
 }
 
-# Create Server 2
-resource "google_compute_instance" "server_2" {
-  name         = "kubernetes-worker-node-1"
-  machine_type = "e2-small"
-  zone         = "asia-southeast1-b"
+#########################################
+# Output Public & Private IPs
+#########################################
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    
-    access_config {}
-  }
-
-  tags = ["http-server", "https-server"]
-
-  metadata = {
-    ssh-keys = file("/Users/test/.ssh/id_ed25519.pub")
-  }
+output "master_public_ip" {
+  value = google_compute_instance.kubernetes_master.network_interface[0].access_config[0].nat_ip
 }
 
-# Create Server 3
-resource "google_compute_instance" "server_3" {
-  name         = "kubernetes-worker-node-2"
-  machine_type = "e2-small"
-  zone         = "asia-southeast1-c"
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    
-    access_config {}
-  }
-
-  tags = ["http-server", "https-server"]
-
-  metadata = {
-    ssh-keys = file("/Users/test/.ssh/id_ed25519.pub")
-  }
-}
-
-# Output for IP addresses
-output "server_1_ip" {
-  value = google_compute_instance.server_1.network_interface[0].access_config[0].nat_ip
-}
-
-output "server_2_ip" {
-  value = google_compute_instance.server_2.network_interface[0].access_config[0].nat_ip
-}
-
-output "server_3_ip" {
-  value = google_compute_instance.server_3.network_interface[0].access_config[0].nat_ip
+output "master_private_ip" {
+  value = google_compute_instance.kubernetes_master.network_interface[0].network_ip
 }
